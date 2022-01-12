3f58ca37f47810e58b5558767c6bc726cc9ebbac
Luis <luisalfonso634@gmail.com>
2022-01-12 13:39:56 -0300

    validations_query

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #Python
 from typing import Optional, List, Dict
+from fastapi.param_functions import Query
 
 #Pydantic
 from pydantic import BaseModel
 
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 
 
@@ -32,3 +33,17 @@ def home():
 def create_person(person: Person = Body(...)):
     return person
     	
+ #Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+): 
+    return {name: age}
+#con los 3 puntos lo estamos haciendo obligatorio, 
+# lo ideal es que un Query no sea obligatorio, sin embargo,
+# es posible, que por alguna razon, nos haga falta hacer 
+# esto en algun momento. Lo ideal es que si es obligatorio, 
+# no sea un Query Parameter, sino un Path Parameter
+
