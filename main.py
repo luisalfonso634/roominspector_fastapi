badafb332c1ca81d6b5c543e9e55b68f570a0892
Luis <luisalfonso634@gmail.com>
2022-01-12 15:42:34 -0300

    validations_body

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ class Person(BaseModel):
     hair_color: Optional[str]  = None
     is_married: Optional[bool] = None
 
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
 
 @app.get("/")
 def home():
@@ -67,3 +72,20 @@ def show_person(
     )
 ):
     return{person_id:"It exists"}
+
+#Validaciones : Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+        person_id: int = Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt=0
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...),
+   ):
+    results = person.dict()
+    results.update(location.dict())
+    return results
