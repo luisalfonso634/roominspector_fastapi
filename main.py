fa6f5e10e34781028c50ba077fa8449d9186798b
Luis <luisalfonso634@gmail.com>
2022-01-12 11:46:49 -0300

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+#Python
+from typing import Optional, List, Dict
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
+
+
 
 app = FastAPI()
 
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    e_mail: str
+    hair_color: Optional[str]  = None
+    is_married: Optional[bool] = None
+
+
 @app.get("/")
 def home():
-    return {"hello":"world"}	
+    return {"hello":"world"}
+
+    # Request and Response Body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
+    	
