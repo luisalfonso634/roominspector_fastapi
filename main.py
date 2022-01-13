0c08b79b42924cff5dcd3fa4f057d5a1a7ea0f84
Luis <luisalfonso634@gmail.com>
2022-01-13 18:42:07 -0300

    automatic_path_and_query_examples

--- main.py
+++ main.py
@@ -118,12 +118,14 @@ def show_person(name: Optional[str] = Query(
         min_length=1, 
         max_length=50,
         title="Person Name",
-        description="This is the person name. It's between 1 and 50 characters"
+        description="This is the person name. It's between 1 and 50 characters",
+        example="Luis Alfonso Fernandez Hernandez"
     ),
     age: str = Query(
         ...,
         title="Person Age",
-        description="This is the Person Age. It's required"
+        description="This is the Person Age. It's required",
+        example=51
     )
 ): 
     return {name: age}
@@ -143,7 +145,8 @@ def show_person(
         ...,
         gt=0,
         title="Person ID",
-        description="This is the Person ID"
+        description="This is the Person ID",
+        example=6341200
     )
 ):
     return{person_id:"It exists"}
@@ -156,10 +159,15 @@ def update_person(
         ...,
         title="Person ID",
         description="This is the person ID",
-        gt=0
+        gt=0,
+        example=6341201
     ),
-    person: Person = Body(...),
-    location: Location = Body(...),
+    person: Person = Body(
+        ...
+        ),
+    location: Location = Body(
+        ...
+        ),
    ):
     results = person.dict()
     results.update(location.dict())
